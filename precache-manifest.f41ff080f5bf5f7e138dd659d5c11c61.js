125837fa7919418f3a0fdd8ce71b0aae36c67dba
devenparekh <devenparekh7@gmail.com>
2020-09-14 13:51:48 -0400

    Updates

---
+++ precache-manifest.f41ff080f5bf5f7e138dd659d5c11c61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1842b02e623da77117c3479222afbfeb",
+    "url": "/Software-Development-Project-Fall-2020/index.html"
+  },
+  {
+    "revision": "5d920bedc72d692f73f6",
+    "url": "/Software-Development-Project-Fall-2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d72361c5ad48539ef9fc",
+    "url": "/Software-Development-Project-Fall-2020/static/js/2.d008ba88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Software-Development-Project-Fall-2020/static/js/2.d008ba88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d920bedc72d692f73f6",
+    "url": "/Software-Development-Project-Fall-2020/static/js/main.678d6e06.chunk.js"
+  },
+  {
+    "revision": "c73b298032cc1755d848",
+    "url": "/Software-Development-Project-Fall-2020/static/js/runtime-main.9b2835e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Software-Development-Project-Fall-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
